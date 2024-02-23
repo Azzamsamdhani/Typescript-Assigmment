db4052368ef0e2245044fc34421cae517b0dc845
Muhammad Azzam <159811992+Azzamsamdhani@users.noreply.github.com>
2024-02-23 10:10:00 -0800

    Add files via upload
    Typescript

---
+++ 20 Empty_Array.ts
@@ -0,0 +1,13 @@
+
+//Think of someting you could store in a array
+let items :string[] = ['Pakistan','karachi','Urdu'];
+
+//Write a program that creates a list containg these items.
+
+let items2 :string[] = [] ;
+
+items2.push('Germany');
+items2.push('England');
+items2.push('India');
+
+console.log(items2);
